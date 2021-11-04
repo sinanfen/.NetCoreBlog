adc4c4089f55879c56413333ea44f3c3a36baef7
Sinan <sinanfen@hotmail.com>
2021-11-04 04:42:35 +0300

    Son derse güncellendi.(Ders:69)

--- CoreDemo/Controllers/RegisterController.cs
+++ CoreDemo/Controllers/RegisterController.cs
@@ -19,7 +19,7 @@ namespace CoreDemo.Controllers
         [HttpGet]
         public IActionResult Index()
         {
-            ViewBag.Cities = GetCityList();
+            //ViewBag.Cities = GetCityList();
             return View();
         }
 
@@ -33,7 +33,7 @@ namespace CoreDemo.Controllers
             {
                 writer.WriterStatus = true;
                 writer.WriterAbout = "Deneme";
-                wm.WriterAdd(writer);
+                wm.TAdd(writer);
                 return RedirectToAction("Index", "Blog");
             }
             else
@@ -48,30 +48,30 @@ namespace CoreDemo.Controllers
 
         }
 
-        public List<SelectListItem> GetCityList()
-        {
-            List<SelectListItem> adminRole = (from x in GetCity()
-                                              select new SelectListItem
-                                              {
-                                                  Text = x,
-                                                  Value = x
-                                              }).ToList();
-            return adminRole;
-        }
+        //public List<SelectListItem> GetCityList()
+        //{
+        //    List<SelectListItem> adminRole = (from x in GetCity()
+        //                                      select new SelectListItem
+        //                                      {
+        //                                          Text = x,
+        //                                          Value = x
+        //                                      }).ToList();
+        //    return adminRole;
+        //}
 
-        public List<string> GetCity()
-        {
-            String[] CitiesArray = new String[] { "Adana", "Adıyaman", "Afyon", "Ağrı", "Aksaray",
-                "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Bartın", "Batman",
-                "Balıkesir", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa",
-                "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ",
-                "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay",
-                "Iğdır", "Isparta", "İçel", "İstanbul", "İzmir", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri",
-                "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa",
-                "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya",
-                "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Şırnak",
-                "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
-            return new List<string>(CitiesArray);
-        }
+        //public List<string> GetCity()
+        //{
+        //    String[] CitiesArray = new String[] { "Adana", "Adıyaman", "Afyon", "Ağrı", "Aksaray",
+        //        "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Bartın", "Batman",
+        //        "Balıkesir", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa",
+        //        "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ",
+        //        "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay",
+        //        "Iğdır", "Isparta", "İçel", "İstanbul", "İzmir", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri",
+        //        "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa",
+        //        "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya",
+        //        "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Şırnak",
+        //        "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+        //    return new List<string>(CitiesArray);
+        //}
     }
 }
