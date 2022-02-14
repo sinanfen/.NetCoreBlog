26f0c719da3935ef33cfabb580e58f83220772ae
sinanfen <sinanfen@hotmail.com>
2022-02-14 22:47:57 +0300

    Güncellendi.(Updated)

---
+++ CoreDemo/Controllers/EmployeeTestController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreDemo.Controllers
+{
+    public class EmployeeTestController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var httpClient = new HttpClient();
+            var responseMessage = await httpClient.GetAsync("https://localhost:44342/api/Default");
+            var jsonString = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<Class1>>(jsonString);
+            return View(values);
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Class1 class1)
+        {
+            var httpClient = new HttpClient();
+            var jsonEmployee = JsonConvert.SerializeObject(class1);
+            StringContent content = new StringContent(jsonEmployee, Encoding.UTF8, "application/json");
+            var responseMessage = await httpClient.PostAsync("https://localhost:44342/api/Default",content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(class1);
+        }
+    }
+
+    public class Class1
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+    }
+}
